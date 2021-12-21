b98b13b7608d299b493a721fb91b7a5c518431f6
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2021-12-21 14:09:00 +0200

    New enhancements!

--- src/main/java/com/cs/ktms/controllers/ClubCoachController.java
+++ src/main/java/com/cs/ktms/controllers/ClubCoachController.java
@@ -11,7 +11,7 @@ import javafx.stage.StageStyle;
 import java.util.Objects;
 
 public class ClubCoachController {
-	public void showScreen(ActionEvent event) {
+	public void showPlayerScreen(ActionEvent event) {
 		try {
 			Stage stage; Scene scene;
 			Parent root = FXMLLoader.load(Objects.requireNonNull(HomeController.class.getResource("PlayerAddition.fxml")));
@@ -19,6 +19,23 @@ public class ClubCoachController {
 			scene = new Scene(root);
 			stage.setScene(scene);
 			stage.setTitle("Player Addition");
+			stage.initStyle(StageStyle.DECORATED);
+			stage.setMaximized(false);
+			stage.setResizable(false);
+			stage.show();
+		}catch (Exception ex){
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	public void showTeamScreen(ActionEvent event) {
+		try {
+			Stage stage; Scene scene;
+			Parent root = FXMLLoader.load(Objects.requireNonNull(ClubCoachController.class.getResource("TeamCreation.fxml")));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.setTitle("Team Creation");
 			stage.initStyle(StageStyle.DECORATED);
 			stage.setMaximized(false);
 			stage.setResizable(false);
---
+++ src/main/java/com/cs/ktms/controllers/TeamController.java
@@ -0,0 +1,4 @@
+package com.cs.ktms.controllers;
+
+public class TeamController {
+}
